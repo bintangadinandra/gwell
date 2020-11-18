fa9d4f0087ec569228277fb3f6b77be2389a812f
bintangadinandra <bintangadinandra@gmail.com>
2020-11-18 16:58:02 +0700

    add users

--- user/user.go
+++ user/user.go
@@ -35,6 +35,42 @@ func Login(request *LoginRequest) bool {
 			UserName: "bitcan01",
 			Password: "b1Tc4NzeR0on3",
 		},
+		&User{
+			UserName: "bitcan02",
+			Password: "8esu6ce4ah",
+		},
+		&User{
+			UserName: "bitcan03",
+			Password: "yh5af6jy5g",
+		},
+		&User{
+			UserName: "bitcan04",
+			Password: "g3qdywpkcy",
+		},
+		&User{
+			UserName: "bitcan05",
+			Password: "x7du9vpuq7",
+		},
+		&User{
+			UserName: "bitcan06",
+			Password: "wt73tcnt6h",
+		},
+		&User{
+			UserName: "bitcan07",
+			Password: "46fusrzemg",
+		},
+		&User{
+			UserName: "bitcan08",
+			Password: "c79k4kbrya",
+		},
+		&User{
+			UserName: "bitcan09",
+			Password: "stqpduc99v",
+		},
+		&User{
+			UserName: "bitcan10",
+			Password: "72zf6xnutg",
+		},
 	}
 	for _, predefinedUser := range predefinedUsers {
 		if predefinedUser.UserName == request.UserName && predefinedUser.Password == request.Password {
